5a4c4af636fe2a4ab397b82b104468bc9a3d619d
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-03-31 01:55:04 +0600

    header part complete

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import image from '../../assets/nizam2.png'
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case text-2xl">Assignment 08</a>
+                </div>
+                <div className="flex-none gap-2">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Knowledge Cafe</a></li>
+                        <li><a>Assignment 07</a></li>
+                        <li><a>Assignment 06</a></li>
+                    </ul>
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src={image} />
+                            </div>
+                        </label>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
